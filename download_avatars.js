bed4cf6675b2fcc23ac53d3b9e5386f4f72ae107
lpenstone <laura.penstone@queensu.ca>
2017-07-03 22:59:08 +0000

    Saving the images to a folder and commenting the code

--- download_avatars.js
+++ download_avatars.js
@@ -1,9 +1,14 @@
 var request = require('request');
+var fs = require('fs');
+
+var myArgs = process.argv.slice(2);
+var owner = myArgs[0];
+var name = myArgs[1];
 
 
-getRepoContributors("jquery", "jquery", function(err, result) {
-  //console.log("Errors:", err);
-  //console.log("Result:", result);
+getRepoContributors(owner, name, function(err, result) {
+  console.log("Errors:", err);
+  console.log("Result:", result);
 });
 
 
@@ -12,7 +17,6 @@ function getRepoContributors(repoOwner, repoName, callback) {
   var GITHUB_TOKEN = "84e700921c00da35fa0bd0e885a827f855b671bd";
   var requestURL = 'https://'+ GITHUB_USER + ':' + GITHUB_TOKEN + '@api.github.com/repos/' + repoOwner + '/' + repoName + '/contributors';
 
-  //console.log(requestURL);
 
   var options = {
     url: requestURL,
@@ -26,19 +30,33 @@ function getRepoContributors(repoOwner, repoName, callback) {
       console.log('Error fetching:', err);
       return
     }
-
-    if (response.statusCode === 200) {
+    if (response.statusCode === 200) { //if the response if requested properly without error
       var json = JSON.parse(body);
-      for (var user in json){
-        console.log(json[user].avatar_url);
+
+      var dir = './avatars'; //Create a new avatars folder if one doesn't exist
+      if (!fs.existsSync(dir)){
+        fs.mkdirSync(dir);
+      }
+
+      for (var user in json){ //get avater URL and name for each contributor
+        var avatar = json[user].avatar_url;
+        var name = json[user].login;
+        downloadImageByURL(avatar, name);
       }
     }
   });
-
-
 }
 
-
-
-
+//Downoad image
+function downloadImageByURL(url, filePath) {
+  request.get(url)
+  .on('error', function (err) { //if there is an error requesting the data
+    throw err;
+  })
+  .on('response', function (response) { //display status of downloads
+    console.log('Response Status Code for ' + filePath + ': ' + response.statusMessage + '\nContent Type: ' + response.headers['content-type']);
+    console.log('Download complete.');
+  })
+  .pipe(fs.createWriteStream('./avatars/' + filePath + '.jpg')); //save avatar images in folder with user's name
+}
 
