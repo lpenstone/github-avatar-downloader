c70973127609f0b9ee6ba4d8b794bad77cb3913d
lpenstone <laura.penstone@queensu.ca>
2017-07-03 18:34:07 +0000

    Adding the request URL

--- download_avatars.js
+++ download_avatars.js
@@ -2,11 +2,16 @@ var request = require('request');
 
 console.log('Welcome to the GitHub Avatar Downloader!');
 
-function getRepoContributors(repoOwner, repoName, callback) {
-  // ...
-}
 
 getRepoContributors("jquery", "jquery", function(err, result) {
   console.log("Errors:", err);
   console.log("Result:", result);
 });
+
+function getRepoContributors(repoOwner, repoName, callback) {
+  var GITHUB_USER = "lpenstone";
+  var GITHUB_TOKEN = "84e700921c00da35fa0bd0e885a827f855b671bd";
+  var requestURL = 'https://'+ GITHUB_USER + ':' + GITHUB_TOKEN + '@api.github.com/repos/' + repoOwner + '/' + repoName + '/contributors';
+
+  console.log(requestURL);
+}
